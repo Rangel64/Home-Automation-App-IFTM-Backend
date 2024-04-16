2e92951c14b3edea1bf96791f5e9dfeab12473c8
Fábio Torres <36510131+Plantere@users.noreply.github.com>
2024-04-16 16:50:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,6 @@ def set_group():
         return {'response':"fail"}
     
 if(__name__ == "__main__"):
-    app.run(port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
 
     
